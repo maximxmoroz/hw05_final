30020606e7973a598a61961799036e21bf8743d0
Максим Морозов <maximxmoroz@MacBook-Air-Maksim.local>
2022-04-24 11:22:37 +0300

    done

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -15,7 +15,8 @@ handler404 = 'core.views.page_not_found'
 handler500 = 'core.views.internal_server_error'
 handler403 = 'core.views.csrf_failure'
 
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
-    )
+if settings.DEBUG: 
+    import debug_toolbar
+    urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
